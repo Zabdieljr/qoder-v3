0cbcc0733af61e280f6be06187e663225b3955f8
zabdieljr <zabdieljr@example.com>
2025-09-07 18:08:21 -0500

    css

---
+++ frontend/eslint.config.js
@@ -0,0 +1,37 @@
+export default {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended"
+  ],
+  ignorePatterns: ["dist", "node_modules"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "18.2"
+    }
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true }
+    ],
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+    node: true
+  }
+}
